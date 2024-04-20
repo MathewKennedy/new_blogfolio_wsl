1941981b995e75399841aa8463d3e2e3f02557d5
Mathew <kennedy4011@outlook.com>
2024-04-20 16:56:13 +0100

    checking out

---
+++ components/mode-toggle.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import React from "react";
+import { useTheme } from "next-themes";
+import { DropdownMenu, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import { Button } from "./ui/button";
+import { SunIcon, MoonIcon } from "@radix-ui/react-icons";
+
+export function ModeToggle(){
+    const { setTheme } = useTheme();
+    return (
+    <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="w-10 px-0">
+                <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"/>
+                <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"/>
+                <span className="sr-only">Toggle theme</span>
+            </Button>
+        </DropdownMenuTrigger>
+    </DropdownMenu>)
+}
