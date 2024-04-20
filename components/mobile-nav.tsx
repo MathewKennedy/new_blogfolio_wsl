ae29e2734a51d6d3500cb2d3c86a3cbac1fb2d3a
Mathew <kennedy4011@outlook.com>
2024-04-20 16:42:26 +0100

    more on mobile nav

--- components/mobile-nav.tsx
+++ components/mobile-nav.tsx
@@ -5,6 +5,10 @@ import { useState } from "react";
 import { Sheet, SheetTrigger, SheetContent } from "./ui/sheet";
 import { Button } from "./ui/button";
 import { HamburgerMenuIcon } from "@radix-ui/react-icons";
+import Link, { LinkProps } from "next/link";
+import { useRouter } from "next/navigation";
+import { Icons } from "./icons";
+import { siteConfig } from "@/config/site";
 
 export function MobileNav(){
     const [open, setOpen] = useState(false);
@@ -17,9 +21,45 @@ export function MobileNav(){
                     <span className="sr-only">Toggle theme</span>
                 </Button>
             </SheetTrigger>
-            <SheetContent>
-                UL
+            <SheetContent side="right">
+                <MobileLink href="/" onOpenChange={setOpen} className="flex items-center space-x-2">
+                    <Icons.logo className="mr-2 h-4 w-4"/>
+                    <span className="font-bold">{siteConfig.name}</span>
+                </MobileLink>
+                <div className="flex flex-col gap-3 mt-3">
+                    <MobileLink onOpenChange={setOpen} href="/blog">Blog</MobileLink>
+                    <MobileLink onOpenChange={setOpen} href="/about">About</MobileLink>
+                    <Link href={siteConfig.links.github} rel="noreferrer">Github</Link>
+                    <Link href={siteConfig.links.linkedin} rel="noreferrer">LinkedIn</Link>
+                </div>
             </SheetContent>
         </Sheet>
     )
+}
+
+interface MobileLinkProps extends LinkProps {
+    children: React.ReactNode;
+    onOpenChange?: (open: boolean) => void
+    className?: string
+}
+
+function MobileLink({
+    href,
+    onOpenChange,
+    children,
+    className,
+    ...props
+} : MobileLinkProps){
+    const router = useRouter();
+    return <Link
+                href={href}
+                onClick={() => {
+                    router.push(href.toString())
+                    onOpenChange?.(false)
+                }}
+                className={className}
+                {...props}
+    >
+        {children}
+    </Link>
 }
