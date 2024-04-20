fd03eeffdc4edd850dd2f3e21fa376c8f38fe256
Mathew <kennedy4011@outlook.com>
2024-04-20 14:17:31 +0100

    icons and nav

---
+++ config/site.ts
@@ -0,0 +1,12 @@
+export const siteConfig = {
+    name: "Mathew Kennedy's Blog",
+    url: "https://example.com",
+    description: "Mathew Kennedy's software and web development blogfolio",
+    author: "Mathew Kennedy",
+    links: {
+        linkedin:"https://www.linkedin.com/in/mathewpkennedy/",
+        github: "https://github.com/MathewKennedy",
+    }
+}
+
+export type SiteConfig = typeof siteConfig
