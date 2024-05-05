610364173d1273c5a0eb56a404f8e8d5e35438c4
Mathew <kennedy4011@outlook.com>
2024-05-05 13:55:42 +0100

    fixing column bug

--- app/tags/[tag]/page.tsx
+++ app/tags/[tag]/page.tsx
@@ -43,7 +43,7 @@ export default function TagPage({ params } : TagPageProps){
                     <h1 className="inline-block font-black text-4xl lg:text-5xl capitalize">{title}</h1>
                 </div>
             </div>
-            <div className="grid cols-12 gap-3 mt-8">
+            <div className="grid grid-cols-12 gap-3 mt-8">
                 <div className="col-span-12 col-start-1 sm:col-span-8">
                     <hr />
                     {displayPosts?.length > 0 ? (
